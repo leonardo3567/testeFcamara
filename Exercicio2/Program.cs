49aefe3bea672160cb0571e05b27fb9ddcbd2fc6
leonardo3567 <leonardo35671@hotmail.com>
2018-08-24 19:27:11 -0300

    Exercicio2 pronto

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -10,15 +10,59 @@ namespace Exercicio2
     {
         static void Main(string[] args)
         {
-            String input;
-            double  inic,fim;
-            Console.WriteLine("Digite o começo do intervalo: ");
-             inic = double.Parse(Console.ReadLine());
+            int inic, fim;
 
-            Console.WriteLine("Digite o fim  do intervalo: ");
-            inic = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Digite o começo do intervalo: ");
+                inic = int.Parse(Console.ReadLine());
 
 
+
+
+                if (inic>0)
+                {
+                    Maior:
+                    Console.WriteLine("Digite o fim  do intervalo: ");
+                    fim = int.Parse(Console.ReadLine());
+
+                    if (fim >inic)
+
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("O fim deve ser maior que o inicio.");
+                        goto Maior;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("O numero deve ser maior que 0");
+                }
+            } while (true);
+            for (int i = inic; i <= fim; i++)
+            {
+                int primo = 0;
+
+                // Console.Write(i);
+                for (int f = 1; f <= i; f++)
+                {
+                    if(i%f == 0)
+                    {
+                        primo++;
+                    }
+                }
+
+                if(primo<=2)
+                { Console.Write(i+" ");
+                }
+               
+            }
+
+            Console.ReadKey();
         }
     }
 }
