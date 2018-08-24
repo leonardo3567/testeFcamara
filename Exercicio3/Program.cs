0783108f507cc20e6c99cd667ac236281b05bbed
leonardo3567 <leonardo35671@hotmail.com>
2018-08-24 20:10:41 -0300

    Exercicio3 pronto

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double qtdHoras, salMin,valorHora, salReceber;
+
+            Console.WriteLine("Digite a quantidade de horas trabalhadas: ");
+            qtdHoras = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do salario minimo: ");
+            salMin = double.Parse(Console.ReadLine());
+
+            valorHora = salMin * 0.10;
+
+            salReceber = valorHora * qtdHoras;
+            salReceber = salReceber - (salReceber*0.03);
+            Console.WriteLine("Você ira receber R${0}", salReceber);
+            Console.ReadKey();
+
+        }
+    }
+}
