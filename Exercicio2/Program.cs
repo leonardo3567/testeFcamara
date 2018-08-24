ed9f6fc2e16e62a6e3bad5870276b68662458397
leonardo3567 <leonardo35671@hotmail.com>
2018-08-24 17:18:19 -0300

    Exe2 inicio

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            double  inic,fim;
+            Console.WriteLine("Digite o começo do intervalo: ");
+             inic = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o fim  do intervalo: ");
+            inic = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
