e228e238c63a0b6ec2f01c439a3ff08c77596c41
leonardo3567 <leonardo35671@hotmail.com>
2018-08-24 16:41:40 -0300

    Exe1 feito

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -10,6 +10,19 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
+            double salFunc = 1000, aumento = 0.015;
+
+            salFunc += salFunc * aumento;
+            Console.WriteLine(salFunc);
+            //loop aumento pensei em 12 anos a partir de 2007
+            for (int i = 0; i < 12; i++)
+            {
+                aumento = aumento * 2;
+                salFunc += salFunc * aumento;
+
+            }
+            Console.WriteLine("Salario atual: " + salFunc);
+            Console.ReadKey();
         }
     }
 }
