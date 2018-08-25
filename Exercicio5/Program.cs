01569a43ad90bce90b06b265e41145daad06bf1f
leonardo3567 <leonardo35671@hotmail.com>
2018-08-25 01:36:47 -0300

    ajuste saldo

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -14,9 +14,9 @@ namespace Exercicio5
             Conta co = new Conta();
             co.numeroConta = 1;
             co.nomeCorrentista = "Leo";
-            co.saldo = 200;
+            co.saldo = 0;
             Console.WriteLine("Nome: {0},Numero de conta: {1}, Saldo: R${2},00",co.nomeCorrentista,co.numeroConta,co.saldo);
-            co.Deposito(130);
+            co.Deposito(330);
             Console.WriteLine("Nome: {0},Numero de conta: {1}, Saldo: R${2},00", co.nomeCorrentista, co.numeroConta, co.saldo);
 
             co.Altera("Leonardo");
