42632d0a6755da448d0b847f495a0ce0657f6ece
leonardo3567 <leonardo35671@hotmail.com>
2018-08-25 01:35:58 -0300

    Exercicio5 completo

---
+++ Exercicio5/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    class Conta
+    {
+        public int numeroConta;
+        public string nomeCorrentista;
+        public double saldo;
+
+
+        //alterarNome, depósito e saque;
+
+        public bool Altera(String nome) {
+            this.nomeCorrentista = nome;
+            return true;
+
+        }
+
+        public bool Deposito(double valor) {
+            this.saldo = this.saldo + valor;
+            return true;
+        }
+
+
+
+        public bool Saque(double valor) {
+            if (this.saldo > valor)
+            {
+                this.saldo = this.saldo - valor;
+                return true;
+            }
+            else {
+                return false; 
+                    }
+        }
+    }
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta co = new Conta();
+            co.numeroConta = 1;
+            co.nomeCorrentista = "Leo";
+            co.saldo = 200;
+            Console.WriteLine("Nome: {0},Numero de conta: {1}, Saldo: R${2},00",co.nomeCorrentista,co.numeroConta,co.saldo);
+            co.Deposito(130);
+            Console.WriteLine("Nome: {0},Numero de conta: {1}, Saldo: R${2},00", co.nomeCorrentista, co.numeroConta, co.saldo);
+
+            co.Altera("Leonardo");
+            co.Saque(200);
+            Console.WriteLine("Nome: {0},Numero de conta: {1}, Saldo: R${2},00", co.nomeCorrentista, co.numeroConta, co.saldo);
+
+            Console.ReadKey();
+        }
+    }
+}
