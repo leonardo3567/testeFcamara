01413b01f352bc667bd6b34f3755bddea3cb4c1e
leonardo3567 <leonardo35671@hotmail.com>
2018-08-25 02:47:34 -0300

    Exercicio6

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nao entendi muito bem esse exe a questao do rodape e do piso me deixaram com duvidas em relacao ao que é pedido
+
+            Retangulo re = new Retangulo();
+
+            Console.WriteLine("Digite o lado A do retangulo: ");
+            double retA = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o lado B do retangulo: ");
+            double retB = double.Parse(Console.ReadLine());
+
+            re.ladoA = retA;
+            re.ladoB = retB;
+
+
+            //assumi o tamanho de um piso como 100cmx100cm
+
+           double  pisos = re.Area();
+
+            Console.WriteLine("Area total:{0}m2", pisos);
+
+            double rodape = re.Perimetro();
+
+            Console.WriteLine("Rodapé: {0}m", rodape);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio6/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Retangulo
+    {
+        public double ladoA;
+        public double ladoB;
+
+        public bool MudaLados(double valor, double valor2)
+        {
+            this.ladoA = valor;
+            this.ladoB = valor2;
+
+            return true;
+        }
+
+        public double RetornarLadoA() {
+
+            return this.ladoA;
+        }
+        public double RetornarLadoB()
+        {
+
+            return this.ladoB;
+        }
+
+        public double Area (){
+            double areat = this.ladoA * this.ladoB;
+
+            return areat;
+}
+
+        public double Perimetro() {
+             double perit = ladoA * 2 + ladoB * 2;
+            return perit;
+             
+        }
+    }
+}
